e23b2630d0dc340324c8a103a44ef1b1eaea9347
JimitParmar <jimit.parmar@somaiya.edu>
2026-04-14 15:07:21 +0530

    ability to change llm model via env

--- src/lib/ai.ts
+++ src/lib/ai.ts
@@ -3,7 +3,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY || "");
 
 const model = genAI.getGenerativeModel({
-  model: "gemini-3.1-flash-lite-preview",
+  model: process.env.MODEL_NAME || "",
 });
 
 export async function aiJSON<T>(
